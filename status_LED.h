d33618a981f65f4d245e0d46835598f477117952
Arthur Vie <arthur.vie@npl.co.uk>
2022-04-25 08:26:27 +0100

    added get status method

--- status_LED.h
+++ status_LED.h
@@ -17,12 +17,14 @@ class status_LED
     public:
     status_LED(uint8_t port, uint8_t pin1, uint8_t pin2, uint8_t pin3);
     void set_status(uint8_t status);
+    uint8_t get_status();
     uint8_t operator=(uint8_t status);
     private:
     uint32_t _port;
     uint32_t _pin1;
     uint32_t _pin2;
     uint32_t _pin3;
+    uint8_t _status;
 };
 
 status_LED::status_LED(uint8_t port, uint8_t pin1, uint8_t pin2, uint8_t pin3)
@@ -35,19 +37,27 @@ status_LED::status_LED(uint8_t port, uint8_t pin1, uint8_t pin2, uint8_t pin3)
     GPIO_SET_MODE(_port, _pin1, OUTPUT);
     GPIO_SET_MODE(_port, _pin2, OUTPUT);
     GPIO_SET_MODE(_port, _pin3, OUTPUT);
+    _status = 0;
 }
 
 void status_LED::set_status(uint8_t status)
 {
     status &= 0b111;
+    _status = status;
     GPIO_WRITE_PIN(_port, _pin1, (status & 0b1));
     GPIO_WRITE_PIN(_port, _pin2, ((status >> 1) & 0b1));
     GPIO_WRITE_PIN(_port, _pin3, ((status >> 2) & 0b1));
 }
 
+uint8_t status_LED::get_status()
+{
+    return _status;
+}
+
 uint8_t status_LED::operator=(uint8_t status)
 {
     status &= 0b111;
+    _status = status;
     GPIO_WRITE_PIN(_port, _pin1, (status & 0b1));
     GPIO_WRITE_PIN(_port, _pin2, ((status >> 1) & 0b1));
     GPIO_WRITE_PIN(_port, _pin3, ((status >> 2) & 0b1));
